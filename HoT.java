f2a2358f380f51b30384e6f000afa0c636e473ed
syuuma1010 <kore.famfam@gmail.com>
2022-07-14 22:59:29 +0900

    HoT: initial revision

---
+++ HoT.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class HoT 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Tossing a coin...\n");
+        Random coin1 = new Random();
+        int c1 = coin1.nextInt(2);
+        Random coin2 = new Random();
+        int c2 = coin2.nextInt(2);
+        Random coin3 = new Random();
+        int c3 = coin3.nextInt(2);
+
+        int heads = 0;
+        int tails = 0;
+
+        System.out.println("Round 1:");
+        if(c1==1){System.out.println("Heads\n"); heads++;}
+        else{System.out.println("Tails\n"); tails++;}
+
+        System.out.println("Round 2:");
+        if(c2==1){System.out.println("Heads\n"); heads++;}
+        else{System.out.println("Tails\n"); tails++;}
+
+        System.out.println("Round 3:");
+        if(c3==1){System.out.println("Heads\n"); heads++;}
+        else{System.out.println("Tails\n"); tails++;}
+
+        System.out.println("Heads: "+heads);
+        System.out.println("Tails: "+tails+"\n");
+
+    }
+}
